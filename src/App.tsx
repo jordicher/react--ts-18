5fad89e919f8b258e4f421780217a2a70c95e980
jordicher <jordi_sodiac@hotmail.com>
2022-04-17 21:01:24 +0200

    concurrency

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+
+interface IResPokemon {
+  name: string;
+}
 function App() {
-  return <div>app</div>;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [response, setResponse] = useState<IResPokemon | null>(null);
+
+  console.log('renders count'); //2
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    //fetch pokeapi
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
+      .then(res => res.json())
+      .then(res => {
+        setResponse(res);
+        setLoading(false);
+      })
+      .catch(err => {
+        setResponse(null);
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+  return <div>{response?.name}</div>;
 }
 
 export default App;
